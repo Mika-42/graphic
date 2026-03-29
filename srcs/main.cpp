88db903033698dd097570c574946f9ac4e4d8818
Matis <matis.mikael@gmail.com>
2026-03-29 15:34:44 +0200

    update example

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,13 +10,24 @@ public:
         : Window(800, 600, "Minimal", std::move(ctx)) {}
 
     void render() override {
+
+		static float t = 0.0f;
+		t += 0.01f;
+
         const glm::mat4 projection = getOrthographicProjection();
-        renderer.setBackgroundColor({1, 1, 1, 1});
+        renderer.setBackgroundColor({0.5, 0.5, 0.5, 1});
         renderer.add({
             .geometry = {80, 80, 240, 120},
+			.radius = glm::vec4(20.0f),
             .backgroundColorA = {0.2f, 0.4f, 0.8f, 1.0f},
             .backgroundColorB = {0.1f, 0.8f, 0.7f, 1.0f},
-            .gradientAngle = 20.0f
+			.borderColor = {1.0f, 1.0f, 1.0f, 1.0f},
+			.shadowColor = {0.0f, 0.0f, 0.0f, 0.5f},
+			.shadowOffset = {5.0f, 5.0f},
+            .gradientAngle = 20.0f,
+			.shadowSoftness = 50.0f,
+			.shadowSpread = 5.0f,
+			.borderThickness = 5.0f
         });
 
 		renderer.add({
@@ -25,7 +36,7 @@ public:
 			.color = {1.0, 0.0, 0.0, 1.0},
 			.gradientColorA = {1.0, 0.0, 0.0, 1.0},
 			.gradientColorB = {0.0, 0.0, 1.0, 1.0},
-			.gradientAngle = 0.0f,
+			.gradientAngle = 360.0f * glm::cos(t),
 			.position = { 100, 100 },
 			.fontSize = 40,
 			.letterSpacing = 3		
